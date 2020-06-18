6781bd16e8928bd2f59000b43cab48d0965cadd4
NFLSGANet <root@nfls.ga>
2020-06-18 15:29:50 -0500

    preliminary integration for illixr

---
+++ Makefile
@@ -0,0 +1,21 @@
+CXX=clang++
+CXXFLAGS=-std=c++2a -Wall -fPIC -I./Core/src
+
+LD_LIBS=-lpthread -pthread
+
+DBG_FLAGS=-g
+OPT_FLAGS=-O3
+
+DBG_SO_NAME=plugin.dbg.so
+OPT_SO_NAME=plugin.opt.so
+
+$(DBG_SO_NAME): CXXFLAGS += $(DBG_FLAGS)
+$(DBG_SO_NAME): ./plugin.cpp ./Core/build/libefusion.so
+	$(CXX) $(CXXFLAGS) $^ -shared -o $@  $(LD_LIBS)
+
+$(OPT_SO_NAME): CXXFLAGS += $(OPT_FLAGS)
+$(OPT_SO_NAME): ./plugin.cpp ./Core/build/libefusion.so
+	$(CXX) $(CXXFLAGS) $^ -shared -o $@  $(LD_LIBS)
+
+clean:
+	rm -rf *.o plugin.dbg.so plugin.opt.so
---
+++ plugin.cpp
@@ -0,0 +1,65 @@
+#include "common/switchboard.hpp"
+#include "common/data_format.hpp"
+#include "data_loading.hpp"
+#include "common/data_format.hpp"
+#include "common/threadloop.hpp"
+
+#include "Core/src/ElasticFusion.h"
+
+#include <pangolin/pangolin.h>
+#include <pangolin/gl/gl.h>
+#include <pangolin/gl/gldraw.h>
+
+using namespace ILLIXR;
+
+#define EF_EPOCH (1.0/10.0)
+
+class elastic_fusion : public ILLIXR::threadloop {
+public:
+	elastic_fusion(phonebook* pb)
+		: _m_sb{pb->lookup_impl<switchboard>()}
+		, _m_cam{sb->subscribe_latest<ef_cam_type>("ef_came")}
+	{
+		// Initialize time
+		sync = std::chrono::high_resolution_clock::now();
+
+		Resolution::getInstance(640, 480);
+    	Intrinsics::getInstance(528, 528, 320, 240);
+		// Create GL context before initializing EF
+		pangolin::Params windowParams;
+    	windowParams.Set("SAMPLE_BUFFERS", 0);
+    	windowParams.Set("SAMPLES", 0);
+    	pangolin::CreateWindowAndBind("Main", 1280, 800, windowParams);
+    	eFusion = new ElasticFusion();
+    	eFusion->processFrame(rgb, depth, timestamp, currentPose, weightMultiplier);
+	}
+
+protected:
+	virtual void _p_one_iteration() override {
+		std::chrono::time_point<std::chrono::system_clock> blockStart = std::chrono::high_resolution_clock::now();
+		if (blockStart < sync) {
+			std::this_thread::yield(); // ←_←
+			// continue;
+			return;
+		}
+		// seconds in double
+		double timespent = std::chrono::duration<double>(blockStart-sync).count();
+		int num_epoch = ceil(timespent/EF_EPOCH);
+		sync += std::chrono::microseconds(num_epoch*((int)(EF_EPOCH*1000000)));
+
+		//TODO: add skipped frames to weight
+		auto most_recent_cam = _m_cam->get_latest_ro();
+
+		Eigen::Matrix4f* currentPose = 0;
+		eFusion->processFrame(most_recent_cam->rgb, most_recent_cam->depth, most_recent_cam->timestamp, currentPose, 1);
+	}
+
+private:
+	switchboard *sb;
+	std::unique_ptr<reader_latest<ef_cam_type>> _m_cam;
+	std::chrono::time_point<std::chrono::system_clock> sync;
+
+	ElasticFusion* eFusion;
+};
+
+PLUGIN_MAIN(elastic_fusion)
