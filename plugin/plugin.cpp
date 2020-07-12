754bf34b33e11a8c9e0a02e9453f0cab1f4f6bc0
Hyjale <saishidual@gmail.com>
2020-07-12 02:20:51 -0400

    initial commit

---
+++ plugin/plugin.cpp
@@ -0,0 +1,75 @@
+#include <pangolin/pangolin.h>
+#include <pangolin/gl/gl.h>
+#include <pangolin/gl/gldraw.h>
+
+#include "common/switchboard.hpp"
+#include "common/data_format.hpp"
+#include "common/threadloop.hpp"
+
+#include <ElasticFusion.h>
+
+using namespace ILLIXR;
+
+#define EF_EPOCH (1.0/10.0)
+
+class elastic_fusion : public ILLIXR::threadloop {
+public:
+	elastic_fusion(const std::string& name_, phonebook* pb_)
+		: threadloop(name_, pb_)
+		, sb{pb->lookup_impl<switchboard>()}
+		, _m_cam{sb->subscribe_latest<rgb_depth_type>("rgb_depth")}
+	{
+		// Initialize time
+		sync = std::chrono::high_resolution_clock::now();
+
+		Resolution::getInstance(672, 376);
+    Intrinsics::getInstance(339.99, 339.99, 322.06, 207.483);
+		// Create GL context before initializing EF
+		pangolin::Params windowParams;
+    windowParams.Set("SAMPLE_BUFFERS", 0);
+    windowParams.Set("SAMPLES", 0);
+
+    pangolin::CreateWindowAndBind("Main", 672, 376, windowParams);
+    eFusion = new ElasticFusion();
+		pangolin::GetBoundWindow()->RemoveCurrent();
+	}
+
+protected:
+	virtual void _p_one_iteration() override {
+		std::chrono::time_point<std::chrono::system_clock> blockStart = std::chrono::high_resolution_clock::now();
+		if (blockStart < sync) {
+			std::this_thread::yield(); // ←_←
+			// continue;
+			return;
+		}
+		// seconds in double
+		double timespent = std::chrono::duration<double>(blockStart-sync).count();
+		int num_epoch = ceil(timespent/EF_EPOCH);
+		sync += std::chrono::microseconds(num_epoch*((int)(EF_EPOCH*1000000)));
+
+		//TODO: add skipped frames to weight
+		auto most_recent_cam = _m_cam->get_latest_ro();
+
+		Eigen::Matrix4f* currentPose = 0;
+		if (most_recent_cam == NULL) {
+			return;
+		}
+
+		if (pangolin::ShouldQuit()){
+			pangolin::BindToContext("Main");
+		}
+
+		eFusion->processFrame(most_recent_cam->rgb, most_recent_cam->depth, most_recent_cam->time, currentPose, 1);
+	}
+
+private:
+	std::shared_ptr<switchboard> sb;
+	std::unique_ptr<reader_latest<rgb_depth_type>> _m_cam;
+	std::chrono::time_point<std::chrono::system_clock> sync;
+
+	ElasticFusion* eFusion;
+};
+
+PLUGIN_MAIN(elastic_fusion)
+
+int main(int argc, char **argv) { return 0; }
