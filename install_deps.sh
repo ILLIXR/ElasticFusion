e9883dd5719dafa569bafd5053902af14ffe8066
Samuel Grayson <sam@samgrayson.me>
2021-09-30 23:59:15 -0500

    Fix install deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
-sudo add-apt-repository ppa:openjdk-r/ppa
+set -e -x
+
+sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get install -y \
      cmake-qt-gui \
      git \
      build-essential \
      libusb-1.0-0-dev \
      libudev-dev \
-     openjdk-7-jdk \
+     openjdk-8-jdk \
      freeglut3-dev \
      libglew-dev \
      libsuitesparse-dev \
